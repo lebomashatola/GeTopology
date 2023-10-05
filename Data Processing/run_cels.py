23c48d6b18bc286ed7ab0c4e900dd725acbfc3ad
Lebo <lebmash50@outlook.com>
2023-10-05 18:54:28 +0200

    Add files via upload

---
+++ Data Processing/run_cels.py
@@ -0,0 +1,10 @@
+import os 
+
+while True:
+    
+    print(" GeTopology (v0.1) \n Welcome to Microarray (.CEL) File Processing!")
+    print("###################################")
+    
+    if opt == 1:
+        os.system('Rscript CELS.r')
+        break
